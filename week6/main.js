d514242c3716e75c40dc7ea4a5d9178a82d5eab9
Serhiy Semerikov <semerikov@gmail.com>
2026-04-28 13:16:57 +0300

    feat(week6): add window resize listener and optimize renderer settings

--- week6/main.js
+++ week6/main.js
@@ -3,10 +3,14 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 
     window.innerWidth / window.innerHeight, 
-    0.1, 1000 );
+    0.1, 9000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+renderer.setClearColor( 0xff0000, 1 );
+
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -15,6 +19,14 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 function animate( time ) {
   renderer.render( scene, camera );
